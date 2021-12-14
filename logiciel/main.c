bfd6c03f51bf22c3a46ae8b376932a72e8890a5b
erwann-lubrano-isen <96076167+erwann-lubrano-isen@users.noreply.github.com>
2021-12-14 14:25:21 +0100

    suppression des membres

--- logiciel/main.c
+++ logiciel/main.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 
 int main(){
+	
+	printf("%d\n",bdd_check_membre(15,20));
+	bdd_supprimer_membre(15,20);
+	printf("%d\n",bdd_check_membre(15,20));
+	bdd_afficher_membres();
 	printf("%d\n",bdd_getSize_table("membre"));
-	printf("%d\n",bdd_check_membre(15,20));
-	bdd_creer_membre(15,20);
-	printf("%d\n",bdd_check_membre(15,20));
+	
 	return 0;
 }
