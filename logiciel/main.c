84af922915fbc74e637f068602ab9eade05d026b
erwann-lubrano-isen <96076167+erwann-lubrano-isen@users.noreply.github.com>
2021-12-14 17:16:07 +0100

    Invitation

--- logiciel/main.c
+++ logiciel/main.c
@@ -1,6 +1,7 @@
 #include "headers/Membre.h"
 #include "headers/Table.h"
 #include "headers/Message.h"
+#include "headers/Invitation.h"
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
@@ -11,11 +12,17 @@ int main(){
 	supprimer_membres_serveur(15);
 	printf("%d\n",bdd_check_membre(15,20));
 	bdd_afficher_membres();
-	printf("%d\n",bdd_getSize_table("membre"));*/
+	printf("%d\n",bdd_getSize_table("membre"));
 	time_t date=time(NULL);
 	bdd_creer_message(1, 1, date, "machin\n");
 	bdd_afficherMessages();
 	bdd_supprimer_message(1, 1, date);
-	bdd_afficherMessages();
+	bdd_afficherMessages();*/
+	
+	bdd_creer_invitation(1,3);
+	bdd_creer_invitation(5,6);
+	
+	bdd_supprimer_invitation(5,6);
+	bdd_afficher_invitation();
 	return 0;
 }
---
+++ logiciel/src/Invitation.c
@@ -0,0 +1,61 @@
+#include "../headers/Invitation.h"
+
+int bdd_creer_invitation(unsigned long int user_id, unsigned long int server_id){
+	FILE * file = NULL;
+	file = fopen("rsc/invitation.dat","a");
+	Invitation invitation;
+	invitation.user_id=user_id;
+	invitation.server_id=server_id;
+	fwrite(&invitation,sizeof(Invitation),1,file);
+	fclose(file);
+	file = NULL;
+	bdd_increment_table("invitation");
+}
+
+void bdd_afficher_invitation(){
+	int size = bdd_getSize_table("invitation");
+	int i =0;
+	Invitation invitation;
+	FILE * file = NULL;
+	file = fopen("rsc/invitation.dat","r");
+	while(fread(&invitation,sizeof(Invitation),1,file) != EOF && i < size){
+		printf("user : %lu\t serveur: %lu\n",invitation.user_id,invitation.server_id);
+		++i;
+	}
+	fclose(file);
+}
+
+int bdd_supprimer_invitation(unsigned long int user_id, unsigned long int server_id){
+	int size = bdd_getSize_table("invitation");
+	if(size==0)return 0;
+	if(size==1){
+		bdd_decrement_table("invitation");
+		return 0;
+	}
+	int i =0;
+	Invitation invitation;
+	Invitation derniereInvitation;
+	FILE * file = NULL;
+	file = fopen("rsc/invitation.dat","r+");
+	fseek(file,sizeof(Invitation)*(size-1),SEEK_SET);
+	fread(&derniereInvitation,sizeof(Invitation),1,file);
+	fseek(file,0,SEEK_SET);
+	while(fread(&invitation,sizeof(Invitation),1,file) != EOF && i < size){
+		++i;
+		if(invitation.user_id==user_id && invitation.server_id == server_id){
+			fseek(file,-sizeof(Invitation),SEEK_CUR);
+			fwrite(&derniereInvitation,sizeof(Invitation),1,file);
+			if(size>=1){
+				bdd_decrement_table("invitation");
+				--size;
+			}
+			--i;
+			fseek(file,sizeof(Invitation)*(size-1),SEEK_SET);
+			fread(&derniereInvitation,sizeof(Invitation),1,file);
+			fseek(file,i*(sizeof(Invitation)),SEEK_SET);
+		}
+		
+	}
+	fclose(file);
+	return 0;
+}
