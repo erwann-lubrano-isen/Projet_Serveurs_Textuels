20cf423c45cf705fcfce5b83e0bc972a8b4c3f0a
Jordan-Pradier <jordan.pradier83@gmail.com>
2021-12-15 11:54:30 +0100

    ajout fonctionalitées salon

--- logiciel/headers/MenuSalon.h
+++ logiciel/headers/MenuSalon.h
@@ -1,6 +1,10 @@
 #ifndef __MENUSALON_H_
 #define __MENUSALON_H_
+#include <string.h>
+#include <time.h>
+#include "Permissions_Salon.h"
+#include "Message.h"
 
 int menuSalon();
-
+void helpSalon();
 #endif
--- logiciel/src/MenuSalon.c
+++ logiciel/src/MenuSalon.c
@@ -1,23 +1,51 @@
 #include "../headers/MenuSalon.h"
-#include <string.h>
+
 
 int menuSalon() {
     char buffer[127];
-    fgets(buffer, 127, stdin);
-    char *commande = strtok(buffer, " ");
-    if(!(strcmp(commande, "!help"))) return 1;
-    if(!(strcmp(commande, "!perm"))) return 2;
-    if(!(strcmp(commande, "!msg"))) return 3;
-    if(!(strcmp(commande, "!exit"))) return 4;
-    if(!(strcmp(commande, "!display"))) return 5;
-    printf("%s: Action inexistante\n", commande);
-    return -1;
+    
+    do{
+    	fgets(buffer, 127, stdin);
+    	char *commande = strtok(buffer, " ");
+    	
+    	if(!(strcmp(commande, "!help"))) helpSalon();
+    	else if(!(strcmp(commande, "!perm"))) permSalon(id_salon);
+    	else if(!(strcmp(commande, "!msg"))) msgSalon(id_salon);
+    	else if(!(strcmp(commande, "!exit"))) return 0;
+    	else if(!(strcmp(commande, "!back"))) return 1;
+    	//else if(!(strcmp(commande, "!display"))) ;//display
+    	else printf("%s: Action inexistante\n", commande);
+    }while(1);    
+    return 0;
 }
-void help(){
+
+void helpSalon(){
 	printf("-------------Voici la liste des commandes--------------\n");
 	printf("!perm rolename perm :  Change les droits pour le salon\n");
-	printf("!msg : Envoyer un message\n");
-	printf("!exit : Quitter le salon\n");
+	printf("!msg text: Envoyer un message\n");
+	printf("!exit : Quitter le programme\n");
+	printf("!back : Retur en arrière\n");
 	printf("!display : Afficher tout les messages\n");
 }
 
+int permSalon(unsigned long int id_salon){
+	char *role = strtok(NULL, " ");
+    	char *perm = strtok(NULL, " ");
+    	if(strlen(role)>30 || strlen(perm)!=2 || perm[0]!='r' && perm[0]!='-' || perm[1]!='w' && perm[1]!='-'){
+    		printf("Commande invalide");
+    		return -1;
+    	}
+	insert_perm_salon(id_salon, role, perms);
+	return 0; 
+}
+
+int msgSalon(unsigned long int id_salon){
+	char *texte = strtok(NULL, " ");
+	if (strlen(texte)>120){
+		printf("Message trop long");
+		return -1;
+	}
+	time_t date=time(NULL);
+	bdd_creer_message(id_salon, id_utilisateur, date, texte);
+	return 0;
+}
