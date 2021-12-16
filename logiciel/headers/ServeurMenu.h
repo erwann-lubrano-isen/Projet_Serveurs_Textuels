79402b215574b7e7d105a7b3de6ca111442aa3a1
Pepitoww <96070621+Pepitoww@users.noreply.github.com>
2021-12-16 16:37:24 +0100

    spotifiac

--- logiciel/headers/ServeurMenu.h
+++ logiciel/headers/ServeurMenu.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "Serveur.h"
+#include "Salon.h"
 #include "Utilisateur.h"
 #include "Invitation.h"
 #include "Demande.h"
@@ -18,8 +19,9 @@ void helpServeur();
 int invitation();
 int createSalon(unsigned long int idServ);
 int deleteSalon(unsigned long int idServ);
-int permServeur(unsigned long int id Serv, unsigned long int idUtilisateur);
+int permServeur(unsigned long int idServ, unsigned long int idUtilisateur);
 int isAdmin(unsigned long int id_user,unsigned long int id_serveur);
 void prompt_serveur(unsigned long int user_id, unsigned long int serveur_id);
+int accept(unsigned long int idServ);
 
 #endif
