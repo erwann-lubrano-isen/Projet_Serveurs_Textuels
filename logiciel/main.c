ca272a93e4711468a1ff9c90852653b787722fdf
erwann-lubrano-isen <96076167+erwann-lubrano-isen@users.noreply.github.com>
2021-12-16 09:03:53 +0100

    main

--- logiciel/main.c
+++ logiciel/main.c
@@ -2,24 +2,9 @@
 #include "headers/Serveur.h"
 #include "headers/Salon.h"
 #include "headers/Utilisateur.h"
+#include "headers/Connexions_Action.h"
 
 int main() {
-	char test[30];
-	char ss[30];
-	printf("PSEUDO : ");
-	scanf("%s", test);
-	printf("\nMDP : ");
-	scanf("%s", ss);
-	printf("\n");
-	bdd_creer_utilisateur(test, ss);
-	bdd_afficher_utilisateur();
-	bd_creationServeur("serveurdebg", 0);
-	bdd_create_Salon("salondebg", 0);
-	if (bdd_Salon_check(0, 0)==1){
-		printf("Salon crée\n");
-	}
-	//bdd_afficher_salon();
-    	menuSalon(0,0,0);
-    	
+    menuConnexion();
 	return 0;
 }
