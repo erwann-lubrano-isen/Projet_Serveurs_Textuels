677e34b74c40c74cc66df54cea104ea65b4120e6
erwann-lubrano-isen <96076167+erwann-lubrano-isen@users.noreply.github.com>
2021-12-24 16:21:01 +0100

    AHHHHHHHHHHHHHHH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- logiciel/headers/Serveur.h
+++ logiciel/headers/Serveur.h
@@ -7,21 +7,59 @@
 #include "Membre.h"
 #include "Salon.h"
 
-
-typedef struct serveur {
+/**
+* @struct Serveur
+* @brief Serveur
+*/
+typedef struct Serveur {
 	unsigned long int id;
 	char nom[30];
 	unsigned long int idProprio;
 } Serveur;
 
 
-
+/**
+* @brief crée un serveur
+* @param nom : nom du serveur
+* @param IdProprio : id du proprietaire du serveur
+*/
 int bd_creationServeur(const char *const nom, unsigned long int IdProprio);
+
+/**
+* @brief supprime un serveur
+* @param id : id du serveur
+*/
 int bd_suppressionServeur(unsigned long int id);
+
+/**
+* @brief supprime tous les serveurs d'un utilisateur
+* @param user_id : id de l'utilisateur
+*/
 int bdd_supprimer_serveur_parId(unsigned long int user_id);
+
+/**
+* @brief supprime un serveur et tous ses membres
+* @param id : id du serveur
+*/
 int bd_suppressionMembresEtServeur(unsigned long int id);
+
+/**
+* @brief renvoie l'id d'un serveur
+* @param nom : nom du serveur
+* @return renvoie 0 si le serveur n'existe pas sinon renvoie son id
+*/
 unsigned long int bdd_getServeur_id(const char * nom);
+
+/**
+* @brief affiche tous les serveurs
+*/
 void bdd_afficher_serveurs();
+
+/**
+* @brief renvoie l'id du propriétaire d'un serveur
+* @param serveur_id : id du serveur
+* @return id du propriétaire
+*/
 unsigned long int bdd_getProprietaireServeur_id(unsigned long int serveur_id);
 
 
--- logiciel/headers/Utilisateur.h
+++ logiciel/headers/Utilisateur.h
@@ -6,16 +6,45 @@
 #include "Demande.h"
 #include "Invitation.h"
 
-typedef struct utilisateur {
+/**
+* @struct Utilisateur
+* @brief utilisateur
+*/
+typedef struct Utilisateur {
 	unsigned long int id;
 	char pseudo[30];
 	char motDePasse[30];
 } Utilisateur;
 
+/**
+* @brief affiche tous les utilisateurs
+*/
 void bdd_afficher_utilisateur();
+
+/**
+* @brief crée un compte utilisateur
+* @param pseudo : nom de l'utilisateur
+* @param motDePasse : mot de passe du compte
+*/
 int bdd_creer_utilisateur(char pseudo[], char motDePasse[]);
+
+/**
+* @brief supprime un compte utilisateur
+* @param id : id de l'utilisateur
+*/
 int bdd_supprimer_utilisateur(unsigned long int id);
+
+/**
+* @brief renvoie l'id d'un utilisateur
+* @param nom : pseudo de l'utilisateur
+* @return renvoie 0 si l'utilisateur n'existe pas sinon renvoie son id
+*/
 unsigned long int bdd_getUtilisateur_id(const char * nom);
+
+/**
+* @brief "renvoie" le pseudo d'un utilisateur
+* @param pseudo : pseudo de l'utilisateur
+*/
 int bdd_getPseudo_utilisateur(char pseudo[], unsigned long int user_id);
 
 #endif
